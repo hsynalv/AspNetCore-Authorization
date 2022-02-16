9571251ed7e136ecfd1b0fc551d4fba1da6cf983
Hasan Hüseyin Alav <alavhasan72892@gmail.com>
2022-02-16 23:34:52 +0300

    FluentValidation Added

---
+++ AuthServer.API/Validations/CreateProductDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using AuthServer.Core.DTOs;
+using FluentValidation;
+
+namespace AuthServer.API.Validations
+{
+    public class CreateProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public CreateProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+
+            RuleFor(x => x.Stock).NotEmpty().WithMessage("Stock is required");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
+        }
+    }
+}
